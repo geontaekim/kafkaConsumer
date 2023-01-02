55f072ff2e1c882c46099b4a35c357da82dbdae9
geontaekim <109645318+geontaekim@users.noreply.github.com>
2023-01-03 08:30:37 +0900

    3333
    3333

---
+++ src/main/java/com/kafka/consumer/KafkaConsumApplication.java
@@ -0,0 +1,17 @@
+package com.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.kafka.consumer")
+@EnableAutoConfiguration
+@SpringBootApplication
+public class KafkaConsumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaConsumApplication.class, args);
+	}
+
+}
